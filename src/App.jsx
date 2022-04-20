0fd32f72f39621f3dc70f90473fa7ef9a4cf9fe0
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-04-20 18:22:07 +0700

    Create Blog Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Nav from './components/nav'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Blog from './pages/Blog'
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </>
   )
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Container } from '../styles/layoutStyled'
+import ReactLoading from 'react-loading';
+function Blog() {
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useState(() => {
+        setLoading(true);
+        axios.get('https://saurav.tech/NewsAPI/sources.json')
+            .then(res => {
+                setPosts(res.data.sources) 
+                setLoading(false);
+            });
+    }, []);
+
+  return (
+    <Container>
+        <h1>Blog</h1>
+        <hr />
+            {posts.map((data, idx) => (
+                <div key={idx}>
+                    <h4>Name: {data.name}</h4>
+                    <p>Description: {data.description}</p>
+                    <p>Url: <a href={data.url} target="_blank" >{data.url}</a></p>
+                    <hr />
+                </div>
+            ))}
+    </Container>
+  )
+}
+
+export default Blog
