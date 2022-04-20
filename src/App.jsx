0fbdef89c33f0a08be9410c95c6ca0c91f08b802
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-04-20 17:52:29 +0700

    Create Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Nav from './components/nav'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
 
   return (
     <>
-      <Nav></Nav>
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
